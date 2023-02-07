1cd4bc6404abfa6503d9dbe47b1a0f9c1615bb54
Tim Huang <ty2huang@edu.uwaterloo.ca>
2023-02-06 21:37:04 -0500

    use git tag for version

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 setup(
     name='codified_json',
     packages=find_packages(include=['codified_json']),
-    version='0.1.0',
     description='Codified-JSON - represent the same JSON data with less characters',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Tim Huang',
     license='MIT',
     install_requires=[],
-    setup_requires=['pytest-runner==6.0.0'],
+    setup_requires=['pytest-runner==6.0.0', 'setuptools_scm'],
+    use_scm_version=True,
     tests_require=['pytest==7.2.0'],
     test_suite='tests'
 )
